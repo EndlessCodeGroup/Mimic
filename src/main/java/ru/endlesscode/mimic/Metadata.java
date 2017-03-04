142d93f169de32d795d53b1d7299173c28e00cec
OsipXD <osip.fatkullin@gmail.com>
2017-03-04 09:30:37 +0300

    Added maven-publish plugin and Metadata annotation

---
+++ src/main/java/ru/endlesscode/mimic/Metadata.java
@@ -0,0 +1,38 @@
+/*
+ * This file is part of MimicAPI.
+ * Copyright (C) 2017 Osip Fatkullin
+ *
+ * MimicAPI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MimicAPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MimicAPI.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ru.endlesscode.mimic;
+
+import org.bukkit.plugin.ServicePriority;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Osip Fatkullin
+ * @since 1.0
+ */
+@Target(value = ElementType.TYPE)
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface Metadata {
+    Class<? extends BaseSystem> system();
+    ServicePriority priority() default ServicePriority.Normal;
+    String[] classes() default {};
+}
