ca0dd7513b5a68b22c91eca42610f5c7ac8a3a68
OsipXD <osip.fatkullin@gmail.com>
2018-05-12 21:00:36 +0300

    Update copyrights

--- bukkit/src/test/java/ru/endlesscode/mimic/bukkit/system/SkillApiTestBase.java
+++ bukkit/src/test/java/ru/endlesscode/mimic/bukkit/system/SkillApiTestBase.java
@@ -1,6 +1,7 @@
 /*
  * This file is part of BukkitMimic.
- * Copyright (C) 2017 EndlessCode Group and contributors
+ * Copyright (C) 2018 Osip Fatkullin
+ * Copyright (C) 2018 EndlessCode Group and contributors
  *
  * BukkitMimic is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
