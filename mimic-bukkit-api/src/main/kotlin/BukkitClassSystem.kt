a96be03acb826f02e5e9dbaebce92e68474e7c3e
Osip Fatkullin <osip.fatkullin@gmail.com>
2020-08-02 15:25:27 +0300

    mimic: Add bukkit-api

---
+++ mimic-bukkit-api/src/main/kotlin/BukkitClassSystem.kt
@@ -0,0 +1,42 @@
+/*
+ * This file is part of BukkitMimic.
+ * Copyright (C) 2018 Osip Fatkullin
+ * Copyright (C) 2018 EndlessCode Group and contributors
+ *
+ * BukkitMimic is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BukkitMimic is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BukkitMimic.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ru.endlesscode.mimic.bukkit
+
+import org.bukkit.entity.Player
+import ru.endlesscode.mimic.ClassSystem
+import ru.endlesscode.mimic.ref.ExistingWeakReference
+
+/** [ClassSystem] adapted for Bukkit. */
+abstract class BukkitClassSystem(player: Player) : ClassSystem {
+
+    val player: Player get() = playerRef.get()
+
+    private val playerRef: ExistingWeakReference<Player> = ExistingWeakReference(player)
+
+    /** Provider of Bukkit class systems. */
+    class Provider(
+        override val id: String,
+        override val isEnabled: Boolean = true,
+        private val constructor: (Player) -> BukkitClassSystem
+    ) : ClassSystem.Provider<BukkitClassSystem> {
+
+        override fun get(arg: Any): BukkitClassSystem = constructor(arg as Player)
+    }
+}
