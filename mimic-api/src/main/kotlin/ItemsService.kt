f999142489f614b51dc359d1327117bd24f8f618
Osip Fatkullin <osip.fatkullin@gmail.com>
2020-08-02 19:19:39 +0300

    mimic-api: Edit ItemsService comments

--- mimic-api/src/main/kotlin/ItemsService.kt
+++ mimic-api/src/main/kotlin/ItemsService.kt
@@ -22,15 +22,15 @@ package ru.endlesscode.mimic
 /** Service for getting custom items by theirs ID. Also can be used to match id with item. */
 interface ItemsService<ItemStackT : Any> : MimicService {
 
-    /** Returns `true` if given [item] represented with given [itemId] in the system. */
+    /** Returns `true` if given [item] represented with given [itemId]. */
     fun isSameItem(item: ItemStackT, itemId: String): Boolean
 
-    /** Returns `true` if item with given [itemId] exists in the item system. */
+    /** Returns `true` if item with given [itemId] exists. */
     fun isItemExists(itemId: String): Boolean
 
-    /** Returns id that represents given [item] in the system, or `null` if the id not found. */
+    /** Returns id that represents given [item], or `null` if the id not found. */
     fun getItemId(item: ItemStackT): String?
 
-    /** Returns item by given [itemId], or `null` if id not found in the system. */
+    /** Returns item by given [itemId], or `null` if id not found. */
     fun getItem(itemId: String): ItemStackT?
 }
