a04711b0c8434347cc69a803be034b3781913f46
Osip Fatkullin <osip.fatkullin@gmail.com>
2021-02-23 15:00:30 +0300

    publish: Configure signing

--- buildSrc/src/main/kotlin/publish.gradle.kts
+++ buildSrc/src/main/kotlin/publish.gradle.kts
@@ -1,28 +1,32 @@
 plugins {
     kotlin("jvm")
     `maven-publish`
+    signing
 }
 
 java {
     withSourcesJar()
 }
 
-configure<PublishingExtension> {
+val isReleaseVersion = !version.toString().endsWith("-SNAPSHOT")
+publishing {
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
         }
     }
 
-    val isReleaseVersion = !version.toString().endsWith("-SNAPSHOT")
     repositories {
-        if (isReleaseVersion) ossrh()
+        if (isReleaseVersion) {
+            maven("https://oss.sonatype.org/service/local/staging/deploy/maven2/") {
+                name = "ossrh"
+                credentials(PasswordCredentials::class)
+            }
+        }
     }
 }
 
-fun RepositoryHandler.ossrh() {
-    maven("https://oss.sonatype.org/service/local/staging/deploy/maven2/") {
-        name = "ossrh"
-        credentials(PasswordCredentials::class)
-    }
+signing {
+    if (hasProperty("signing.gnupg.keyName")) useGpgCmd()
+    sign(publishing.publications["maven"])
 }
