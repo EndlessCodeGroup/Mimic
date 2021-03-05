fadf1a8598178910c3797bf21b8dc3aafee6b5e0
Osip Fatkullin <osip.fatkullin@gmail.com>
2021-03-05 20:50:39 +0300

    publish: Add POM configuration

--- buildSrc/src/main/kotlin/publish.gradle.kts
+++ buildSrc/src/main/kotlin/publish.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
-    kotlin("jvm")
+    `java-base`
     `maven-publish`
     signing
 }
 
 java {
     withSourcesJar()
+    withJavadocJar()
 }
 
 val isReleaseVersion = !version.toString().endsWith("-SNAPSHOT")
@@ -13,6 +14,34 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
+
+            pom {
+                val gitHubProject = "EndlessCodeGroup/Mimic"
+                name.set(provider { project.name })
+                description.set(provider { project.description })
+                url.set("https://github.com/$gitHubProject/")
+
+                scm {
+                    url.set("https://github.com/$gitHubProject/")
+                    connection.set("scm:git:git://github.com/$gitHubProject.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:$gitHubProject.git")
+                }
+
+                licenses {
+                    license {
+                        name.set("GNU Affero General Public License v3.0")
+                        url.set("https://choosealicense.com/licenses/agpl-3.0/")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("osipxd")
+                        name.set("Osip Fatkullin")
+                        email.set("osip.fatkullin@gmail.com")
+                    }
+                }
+            }
         }
     }
 
