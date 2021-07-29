148e2d101a38f764ec7a4656cae461b099851660
Osip Fatkullin <osip.fatkullin@gmail.com>
2021-07-29 20:27:39 +0300

    api: Add api dump generation

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,6 +10,7 @@ java {
 dependencies {
     implementation(kotlin("gradle-plugin", version = "1.5.21"))
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.5.0")
+    implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.6.0")
     implementation("de.undercouch:gradle-download-task:4.1.2")
 }
 
--- buildSrc/src/main/kotlin/commons.gradle.kts
+++ buildSrc/src/main/kotlin/commons.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm")
     id("org.jetbrains.dokka")
+    id("org.jetbrains.kotlinx.binary-compatibility-validator")
 }
 
 tasks.withType<JavaCompile> {
