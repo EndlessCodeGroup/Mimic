aef8678011cc1f1d54a807a3e4f1239b0b456eca
Osip Fatkullin <osip.fatkullin@gmail.com>
2021-08-19 09:06:53 +0300

    build: Update binary-compatibility-validator to 0.7.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     implementation(kotlin("gradle-plugin", version = kotlinVersion))
     implementation(kotlin("serialization", version = kotlinVersion))
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.5.0")
-    implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.6.0")
+    implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.7.0")
     implementation("de.undercouch:gradle-download-task:4.1.2")
 }
 
