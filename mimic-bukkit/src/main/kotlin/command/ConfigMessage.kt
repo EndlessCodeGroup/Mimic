a5a2728610fcb203c415417cd849abc5ee02bd61
Osip Fatkullin <osip.fatkullin@gmail.com>
2022-04-24 22:13:27 +0300

    refactor(config): Add multi-choice config message

--- mimic-bukkit/src/main/kotlin/command/ConfigMessage.kt
+++ mimic-bukkit/src/main/kotlin/command/ConfigMessage.kt
@@ -10,14 +10,16 @@ import net.kyori.adventure.text.format.TextDecoration
 import ru.endlesscode.mimic.ExperimentalMimicApi
 import ru.endlesscode.mimic.Mimic
 import ru.endlesscode.mimic.MimicService
+import ru.endlesscode.mimic.config.ConfigProperty
 import ru.endlesscode.mimic.config.MimicConfig
 import ru.endlesscode.mimic.config.StringConfigProperty
+import ru.endlesscode.mimic.config.StringSetConfigProperty
 import ru.endlesscode.mimic.internal.append
 import ru.endlesscode.mimic.internal.appendLine
 import ru.endlesscode.mimic.internal.buildTextComponent
 
 @OptIn(ExperimentalMimicApi::class)
-internal fun buildConfigMessage(mimic: Mimic): TextComponent = buildTextComponent {
+internal fun buildConfigMessage(mimic: Mimic, config: MimicConfig): TextComponent = buildTextComponent {
     appendSelectablePropertyConfig(
         property = MimicConfig.LEVEL_SYSTEM,
         selectedId = mimic.getLevelSystemProvider().id,
@@ -33,49 +35,90 @@ internal fun buildConfigMessage(mimic: Mimic): TextComponent = buildTextComponen
         selectedId = mimic.getPlayerInventoryProvider().id,
         services = mimic.getAllPlayerInventoryProviders(),
     )
+    appendSetPropertyConfig(
+        property = MimicConfig.DISABLED_ITEMS_REGISTRIES,
+        values = config.disabledItemsRegistries,
+        services = mimic.getAllItemsRegistries(),
+    )
 }
 
 private fun TextComponent.Builder.appendSelectablePropertyConfig(
     property: StringConfigProperty,
     selectedId: String,
     services: Map<String, MimicService>,
-) {
-    appendLine()
-    appendComment(property.formattedTitle)
-    val availableOptions = (services - selectedId).values
+) = appendProperty(property) {
+    val availableOptions = services.keys - selectedId
     if (availableOptions.isNotEmpty()) {
         appendLine()
-        appendComment("Available options: ")
-        append(
-            availableOptions.toSelectableOptions(
-                hint = "Click to select this option",
-                command = "/mimic info",
-                color = NamedTextColor.GRAY,
-            )
+        appendComment("Options: ")
+        appendSelectableOptions(
+            availableOptions,
+            hint = "Click to select this option",
+            command = "/mimic info",
+            color = NamedTextColor.GRAY,
         )
     }
 
-    append("\n${property.path}: ", NamedTextColor.GREEN)
+    appendLine()
+    appendPropertyPath(property)
     append(selectedId)
+}
+
+private fun TextComponent.Builder.appendSetPropertyConfig(
+    property: StringSetConfigProperty,
+    values: Set<String>,
+    services: Map<String, MimicService>,
+) = appendProperty(property) {
+    val selectableIds = services.keys - values
+    appendLine()
+    appendComment("Options: ")
+    appendSelectableOptions(
+        selectableIds,
+        hint = "Click to add",
+        command = "/mimic info",
+        color = NamedTextColor.GRAY,
+    )
+
+    appendLine()
+    appendPropertyPath(property)
+    append("[")
+    appendSelectableOptions(
+        values,
+        hint = "Click to remove",
+        command = "/mimic info",
+    )
+    append("]")
+}
+
+private fun TextComponent.Builder.appendProperty(property: ConfigProperty<*>, body: TextComponent.Builder.() -> Unit) {
+    appendLine()
+    appendComment(property.formattedTitle)
+    body()
     appendLine()
 }
 
-private fun Collection<MimicService>.toSelectableOptions(
+private fun TextComponent.Builder.appendSelectableOptions(
+    options: Collection<String>,
     hint: String,
     command: String,
     color: TextColor? = null,
-) = mapIndexed { index, service ->
-    buildTextComponent {
-        color(color)
-        if (index != 0) append(", ")
-        appendClickable(service.id, hint, command)
+) = append(
+    options.mapIndexed { index, option ->
+        buildTextComponent {
+            color(color)
+            if (index != 0) append(", ")
+            appendClickable(option, hint, command)
+        }
     }
-}
+)
 
 private fun TextComponent.Builder.appendClickable(text: String, hint: String, command: String) {
     append(text, TextDecoration.UNDERLINED)
     hoverEvent(HoverEvent.showText(Component.text(hint)))
     clickEvent(ClickEvent.runCommand(command))
 }
+
+private fun TextComponent.Builder.appendPropertyPath(property: ConfigProperty<*>) =
+    append("${property.path}: ", NamedTextColor.GREEN)
 
 private fun TextComponent.Builder.appendComment(comment: String) = append("# $comment", NamedTextColor.GRAY)
