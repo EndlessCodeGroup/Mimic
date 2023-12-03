e0bc37ab6e125797ad158849c492daddbb04ec69
Osip Fatkullin <osip.fatkullin@gmail.com>
2023-12-03 21:55:09 +0100

    fix(build): Remove generate accessor import

--- buildSrc/src/main/kotlin/internal/libs.kt
+++ buildSrc/src/main/kotlin/internal/libs.kt
@@ -1,6 +1,5 @@
 package internal
 
-import gradle.kotlin.dsl.accessors._1f737d11fad22b9b058419dfc437a798.versionCatalogs
 import org.gradle.api.Project
 import org.gradle.api.artifacts.MinimalExternalModuleDependency
 import org.gradle.api.provider.Provider
@@ -14,24 +13,24 @@ internal object libs {
 
     context(Project)
     val junit_bom
-        get() = get("junit-bom")
+        get() = lib("junit-bom")
 
     context(Project)
     val junit_jupiter
-        get() = get("junit-jupiter")
+        get() = lib("junit-jupiter")
 
     context(Project)
     val junit_jupiter_params
-        get() = get("junit-jupiter-params")
+        get() = lib("junit-jupiter-params")
 
     context(Project)
     val kotest_assertions
-        get() = get("kotest-assertions")
+        get() = lib("kotest-assertions")
 
     context(Project)
     val mockk
-        get() = get("mockk")
+        get() = lib("mockk")
 
-    private fun Project.get(alias: String): Provider<MinimalExternalModuleDependency> =
+    private fun Project.lib(alias: String): Provider<MinimalExternalModuleDependency> =
         versionCatalogs.named("libs").findLibrary(alias).get()
 }
