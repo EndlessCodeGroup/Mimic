63d0bc13441097c2c9e2656b13f2a55c19fad845
Osip Fatkullin <osip.fatkullin@gmail.com>
2025-04-23 10:03:21 +0200

    chore: Remove redundant compatibility layers

--- mimic-bukkit/src/main/kotlin/internal/Configuration.kt
+++ mimic-bukkit/src/main/kotlin/internal/Configuration.kt
@@ -3,13 +3,8 @@ package ru.endlesscode.mimic.internal
 import org.bukkit.configuration.Configuration
 import org.bukkit.configuration.file.FileConfigurationOptions
 
-@Suppress("DEPRECATION") // Changed in 1.18.1
 internal fun FileConfigurationOptions.setHeader(vararg lines: String) {
-    callCompat(
-        "FileConfigurationOptions.setHeader",
-        block =  { setHeader(lines.asList()) },
-        compat = { header(lines.joinToString("\n")) },
-    )
+    setHeader(lines.asList())
 }
 
 internal fun Configuration.applyDefaults() {
@@ -17,11 +12,6 @@ internal fun Configuration.applyDefaults() {
     for (key in defaults.getKeys(true)) set(key, get(key))
 }
 
-// Added in 1.18.1
 internal fun Configuration.setComments(path: String, vararg comments: String?) {
-    callCompat(
-        "Configuration.setComments",
-        block = { setComments(path, comments.asList()) },
-        compat = { /* no-op */ },
-    )
+    setComments(path, comments.asList())
 }
